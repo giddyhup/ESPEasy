58bf4ca10f79e1af6cf07a887dbcf8bd60c3d13e
giddyhup <D:\Users\mole\Mail\The Bat>
2020-08-04 20:52:24 +0200

    Custom bin including IR receiving

--- tools/pio/pre_custom_esp82xx.py
+++ tools/pio/pre_custom_esp82xx.py
@@ -20,32 +20,35 @@ if os.path.isfile('src/Custom.h'):
   env.Append(CPPDEFINES=["USE_CUSTOM_H"])
 else:
   env.Append(CPPDEFINES=[
-    "CONTROLLER_SET_ALL",
+    "CONTROLLER_SET_STABLE",
     "NOTIFIER_SET_NONE",
     "PLUGIN_SET_ONLY_SWITCH",
     "USES_P001",  # Switch
     "USES_P002",  # ADC
     "USES_P004",  # Dallas DS18b20
-    "USES_P027",  # INA219
+    "USES_P015",  # Light/Lux - TSL2561
+    "USES_P016",  # Communication - TSOP4838
     "USES_P028",  # BME280
-    "USES_P036",  # FrameOLED
-    "USES_P045",  # MPU6050
-    "USES_P049",  # MHZ19
-    "USES_P052",  # SenseAir
-    "USES_P056",  # SDS011-Dust
-    "USES_P059",  # Encoder
-    "USES_P081",  # Cron
-    "USES_P082",  # GPS
-    "USES_P085",  # AcuDC24x
+    "USES_P030",  # Environment - BMP280
+    "USES_P037",  # Generic - MQTT Import
+    "USES_P038",  # Output - NeoPixel (Basic)
+#    "USES_P045",  # MPU6050
+#    "USES_P049",  # MHZ19
+#    "USES_P052",  # SenseAir
+#    "USES_P056",  # SDS011-Dust
+#    "USES_P059",  # Encoder
+#    "USES_P081",  # Cron
+#    "USES_P082",  # GPS
+#    "USES_P085",  # AcuDC24x
 #   "USES_P087",  # Serial Proxy
 #   "USES_P094",  # CUL Reader
 #   "USES_P095",  # TFT ILI9341
-
-    "USES_C016",  # Cache Controller
-    "USES_C018",  # TTN/RN2483
+#    "USES_C016",  # Cache Controller
+#    "USES_C001",  # Domo
+#    "USES_C005",  # OpenHab
 
     "FEATURE_MDNS",
-    "FEATURE_SD",
+#    "FEATURE_SD",
 
     "USE_SETTINGS_ARCHIVE"
   ])
